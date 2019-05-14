57233e09172ac141c150e9b8e1debbe636334748
Oliveira <eliaspradoprofessional@outlook.com>
2019-05-14 19:21:59 +0000

    added some button functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,5 +9,22 @@ var game = {
     dark: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'), 
     green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
   },
-  strict: false,
+  strict: false
 }
+
+
+function btnBlue() {
+  document.getElementById("blue").style.backgroundColor="white";
+}
+function btnGreen() {
+  document.getElementById("green").style.backgroundColor="white";
+}
+function btnRed() {
+  document.getElementById("red").style.backgroundColor="white";
+}
+
+function btnYellow() {
+  document.getElementById("yellow").style.backgroundColor="white";
+}
+
+
