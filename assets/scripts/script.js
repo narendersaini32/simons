3e5204641f9c710eec5cbd88b882f51f4c8d93f4
Oliveira <eliaspradoprofessional@outlook.com>
2019-05-17 01:02:48 +0000

    fixing variables and DOM queries

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,18 @@
-var game = {
-  count: 0,
-  possibilities: ['#green', '#blue', '#red', '#dark'],
-  currentGame: [],
-  player: [],
-  sound: {
-    blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
-    red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
-    dark: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
-    green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
-  },
-  strict: false
-}
 
+let sound = true;
+let blueBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+let redBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+let yellowBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+let greenBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 
-function btnBlue() {
+// VARIABLES - DOM QUERIES
+
+const btnBlue = document.querySelectorAll("btnBlue");
+const btnGreen = document.querySelectorAll("btnGreen");
+const btnRed = document.querySelectorAll("btnRed");
+const btnYellow = document.querySelectorAll("btnYellow");
+
+/*function btnBlue() {
   document.getElementById("blue").style.backgroundColor = "white";
 }
 
@@ -27,6 +26,31 @@ function btnRed() {
 
 function btnYellow() {
   document.getElementById("yellow").style.backgroundColor = "white";
+}*/
+
+function buttonEffect(value) {
+  if (sound) {
+    switch (value) {
+      case 1:
+        blueBtnAudio.play();
+        btnBlue.style.backgroundColor = "#66AFF8"; //Soft blue   
+        break;
+      case 2:
+        greenBtnAudio.play();
+        btnGreen.style.backgroundColor = "#29FF9E"; //  Brighter green    
+        break;
+      case 3:
+        redBtnAudio.play();
+        btnRed.style.backgroundColor = "#E07070"; // Soft red
+        break;
+      case 4:
+        yellowBtnAudio.play();
+        btnYellow.style.backgroundColor = "#FAD362"; // Soft orange
+        break;
+      default:
+        return false;
+    }
+  }
+  sound = true;
 }
-
-
+console.log(blueBtnAudio.play());
