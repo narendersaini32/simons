44cf91dc2edc581bafbb8980a7b561a07a16f113
Oliveira <eliaspradoprofessional@outlook.com>
2019-05-20 17:21:05 +0000

    declaring button effects into vars

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,3 @@
-
 let blueBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
 let redBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
 let yellowBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
@@ -13,38 +12,45 @@ const btnYellow = document.querySelectorAll("#btnYellow");
 
 
 $(document).ready(function() {
-//button effects 
+  //button effects 
 
-//button blue effect
-  $(btnBlue).click(function() {
-	var originalColor = $(this).css('background-color');
-  blueBtnAudio.play();
-  $(this).css('background-color', '#00FFFF');	
-  setTimeout(function() {
-  	$(btnBlue).css('background-color', originalColor);
-  }, 100);  
-});
-  
+  //button blue effect
+  var blueButtonEffect = $(btnBlue).click(function() {
+    var originalColor = $(this).css('background-color');
+    blueBtnAudio.play();
+    $(this).css('background-color', '#00FFFF');
+    setTimeout(function() {
+      $(btnBlue).css('background-color', originalColor);
+    }, 100);
+  });
 
-//button green effect
-  $(btnGreen).click(function() {
+
+  //button green effect
+  var greenButtonEffect = $(btnGreen).click(function() {
     var originalColor = $(this).css('background-color');
     greenBtnAudio.play();
     $(this).css('background-color', '#7FFF00');
-    setTimeout(function(){
+    setTimeout(function() {
       $(btnGreen).css('background-color', originalColor);
     }, 100)
   });
 
-  $(btnRed).click(function() {
-    var originalColor = $(this).css('background-color', originalColor);
-    $(this).css('background-color', '#F08080');
+  var redButtonEffect = $(btnRed).click(function() {
+    var originalColor = $(this).css('background-color');
     redBtnAudio.play();
+    $(this).css('background-color', '#F08080');
+    setTimeout(function() {
+      $(btnRed).css('background-color', originalColor)
+    }, 100);
   });
 
-  $(btnYellow).click(function() {
-    $(this).css('background-color', '#F0E68C');
+  var yellowButtonEffect = $(btnYellow).click(function() {
+    var originalColor = $(this).css('background-color');
     yellowBtnAudio.play();
+    $(this).css('background-color', '#F0E68C');
+    setTimeout(function() {
+      $(btnYellow).css('background-color', originalColor)
+    }, 100);
   });
 
 });
