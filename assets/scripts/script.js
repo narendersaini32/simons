e072fb0e00a9d310871977c1967a28441a364031
Oliveira <eliaspradoprofessional@outlook.com>
2019-05-20 23:32:12 +0000

    fixed a bug (id without a #)

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,57 +5,60 @@ let greenBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4
 
 // VARIABLES - DOM QUERIES
 
-const btnBlue = document.querySelectorAll("#btnBlue");
-const btnGreen = document.querySelectorAll("#btnGreen");
-const btnRed = document.querySelectorAll("#btnRed");
-const btnYellow = document.querySelectorAll("#btnYellow");
+const btnBlue = "#btnBlue";
+const btnGreen = "#btnGreen";
+const btnRed = "#btnRed";
+const btnYellow = "#btnYellow";
 
-const startButton = document.querySelectorAll("startButton");
-console.log(startButton);
+const startButton = "#startButton";
+
 //jQuery
 $(document).ready(function() {
 
-  function startGame() {
-    if ($(startButton).click()) {
-      //button effects 
-      //button blue effect
-      var blueButtonEffect = $(btnBlue).click(function() {
-        var originalColor = $(this).css('background-color');
-        blueBtnAudio.play();
-        $(this).css('background-color', '#00FFFF');
-        setTimeout(function() {
-          $(btnBlue).css('background-color', originalColor);
-        }, 100);
-      });
-
-      //button green effect
-      var greenButtonEffect = $(btnGreen).click(function() {
-        var originalColor = $(this).css('background-color');
-        greenBtnAudio.play();
-        $(this).css('background-color', '#7FFF00');
-        setTimeout(function() {
-          $(btnGreen).css('background-color', originalColor);
-        }, 100)
-      });
-      // button red effect
-      var redButtonEffect = $(btnRed).click(function() {
-        var originalColor = $(this).css('background-color');
-        redBtnAudio.play();
-        $(this).css('background-color', '#F08080');
-        setTimeout(function() {
-          $(btnRed).css('background-color', originalColor)
-        }, 100);
-      });
-      // button yellow effect
-      var yellowButtonEffect = $(btnYellow).click(function() {
-        var originalColor = $(this).css('background-color');
-        yellowBtnAudio.play();
-        $(this).css('background-color', '#F0E68C');
-        setTimeout(function() {
-          $(btnYellow).css('background-color', originalColor)
-        }, 100);
-      });
-    }
+  function buttonEffects() {
+    //button effects 
+    //button blue effect
+    var blueButtonEffect = $(btnBlue).click(function() {
+      var originalColor = $(this).css('background-color');
+      blueBtnAudio.play();
+      $(this).css('background-color', '#00FFFF');
+      setTimeout(function() {
+        $(btnBlue).css('background-color', originalColor);
+      }, 100);
+    });
+    //button green effect
+    var greenButtonEffect = $(btnGreen).click(function() {
+      var originalColor = $(this).css('background-color');
+      greenBtnAudio.play();
+      $(this).css('background-color', '#7FFF00');
+      setTimeout(function() {
+        $(btnGreen).css('background-color', originalColor);
+      }, 100)
+    });
+    // button red effect
+    var redButtonEffect = $(btnRed).click(function() {
+      var originalColor = $(this).css('background-color');
+      redBtnAudio.play();
+      $(this).css('background-color', '#F08080');
+      setTimeout(function() {
+        $(btnRed).css('background-color', originalColor)
+      }, 100);
+    });
+    // button yellow effect
+    var yellowButtonEffect = $(btnYellow).click(function() {
+      var originalColor = $(this).css('background-color');
+      yellowBtnAudio.play();
+      $(this).css('background-color', '#F0E68C');
+      setTimeout(function() {
+        $(btnYellow).css('background-color', originalColor)
+      }, 100);
+    });
   }
+
+  function startGame() {// it has a bug if clicked twice!
+      $(startButton).on('click', buttonEffects);
+    };
+
+  
 });
 // you will have to create more four functions like the above..
