1eb63207ac3f44e5b59a4fa7a0efd56f7e0982c3
Oliveira <eliaspradoprofessional@outlook.com>
2019-05-20 18:35:34 +0000

    trying to create a fucntion that will on/off the button effects

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,48 +10,52 @@ const btnGreen = document.querySelectorAll("#btnGreen");
 const btnRed = document.querySelectorAll("#btnRed");
 const btnYellow = document.querySelectorAll("#btnYellow");
 
+const startButton = document.querySelectorAll("startButton");
+console.log(startButton);
 //jQuery
 $(document).ready(function() {
-  //button effects 
 
-  //button blue effect
-  var blueButtonEffect = $(btnBlue).click(function() {
-    var originalColor = $(this).css('background-color');
-    blueBtnAudio.play();
-    $(this).css('background-color', '#00FFFF');
-    setTimeout(function() {
-      $(btnBlue).css('background-color', originalColor);
-    }, 100);
-  });
+  function startGame() {
+    if ($(startButton).click()) {
+      //button effects 
+      //button blue effect
+      var blueButtonEffect = $(btnBlue).click(function() {
+        var originalColor = $(this).css('background-color');
+        blueBtnAudio.play();
+        $(this).css('background-color', '#00FFFF');
+        setTimeout(function() {
+          $(btnBlue).css('background-color', originalColor);
+        }, 100);
+      });
 
-
-  //button green effect
-  var greenButtonEffect = $(btnGreen).click(function() {
-    var originalColor = $(this).css('background-color');
-    greenBtnAudio.play();
-    $(this).css('background-color', '#7FFF00');
-    setTimeout(function() {
-      $(btnGreen).css('background-color', originalColor);
-    }, 100)
-  });
-
-  var redButtonEffect = $(btnRed).click(function() {
-    var originalColor = $(this).css('background-color');
-    redBtnAudio.play();
-    $(this).css('background-color', '#F08080');
-    setTimeout(function() {
-      $(btnRed).css('background-color', originalColor)
-    }, 100);
-  });
-
-  var yellowButtonEffect = $(btnYellow).click(function() {
-    var originalColor = $(this).css('background-color');
-    yellowBtnAudio.play();
-    $(this).css('background-color', '#F0E68C');
-    setTimeout(function() {
-      $(btnYellow).css('background-color', originalColor)
-    }, 100);
-  });
-
+      //button green effect
+      var greenButtonEffect = $(btnGreen).click(function() {
+        var originalColor = $(this).css('background-color');
+        greenBtnAudio.play();
+        $(this).css('background-color', '#7FFF00');
+        setTimeout(function() {
+          $(btnGreen).css('background-color', originalColor);
+        }, 100)
+      });
+      // button red effect
+      var redButtonEffect = $(btnRed).click(function() {
+        var originalColor = $(this).css('background-color');
+        redBtnAudio.play();
+        $(this).css('background-color', '#F08080');
+        setTimeout(function() {
+          $(btnRed).css('background-color', originalColor)
+        }, 100);
+      });
+      // button yellow effect
+      var yellowButtonEffect = $(btnYellow).click(function() {
+        var originalColor = $(this).css('background-color');
+        yellowBtnAudio.play();
+        $(this).css('background-color', '#F0E68C');
+        setTimeout(function() {
+          $(btnYellow).css('background-color', originalColor)
+        }, 100);
+      });
+    }
+  }
 });
 // you will have to create more four functions like the above..
