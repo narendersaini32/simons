36989a633b23e491de5eeb14f15daca85fd14b05
Oliveira <eliaspradoprofessional@outlook.com>
2019-05-22 15:25:11 +0000

    making the clickByItself() function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -15,91 +15,76 @@ $(document).ready(function() {
   const btnRed = "#btnRed";
   const btnYellow = "#btnYellow";
   const startButton = "#startButton";
-  
-  const gameButton = ".gameButton";
-  
-  //objects from buttonEffect() function
-
-  const effects = buttonEffects();
- 
-
+  const gameButton = ".gameButton";//button start
+  const effects = buttonEffects();//zombie code//objects from buttonEffect() function
 
   //button effects 
- 
-    //button blue effect
-    var blueButtonEffect = $(btnBlue).click(function() {
-      var originalColor = $(this).css('background-color');
-      blueBtnAudio.play();
-      $(this).css('background-color', '#00FFFF');
-      setTimeout(function() {
-        $(btnBlue).css('background-color', originalColor);
-      }, 100);
-    });
-    //button green effect
-    var greenButtonEffect = $(btnGreen).click(function() {
-      var originalColor = $(this).css('background-color');
-      greenBtnAudio.play();
-      $(this).css('background-color', '#7FFF00');
-      setTimeout(function() {
-        $(btnGreen).css('background-color', originalColor);
-      }, 100);
-    });
-    // button red effect
-    var redButtonEffect = $(btnRed).click(function() {
-      var originalColor = $(this).css('background-color');
-      redBtnAudio.play();
-      $(this).css('background-color', '#F08080');
-      setTimeout(function() {
-        $(btnRed).css('background-color', originalColor)
-      }, 100);
-    });
-    // button yellow effect
-    var yellowButtonEffect = $(btnYellow).click(function() {
-      var originalColor = $(this).css('background-color');
-      yellowBtnAudio.play();
-      $(this).css('background-color', '#F0E68C');
-      setTimeout(function() {
-        $(btnYellow).css('background-color', originalColor)
-      }, 100);
-    });
-    
-console.log(blueButtonEffect);
-    
-    
-    function buttonEffects(){
-     
-    }
-    
-    
-    
-    /*return  {
-      item1: blueButtonEffect,
-      item2: redButtonEffect,
-      item3: greenButtonEffect,
-      item4: yellowButtonEffect
-    };*/
+
+  //button blue effect
+  var blueButtonEffect = $(btnBlue).click(function() {
+    var originalColor = $(this).css('background-color');
+    blueBtnAudio.play();
+    $(this).css('background-color', '#00FFFF');
+    setTimeout(function() {
+      $(btnBlue).css('background-color', originalColor);
+    }, 100);
+  });
+  //button green effect
+  var greenButtonEffect = $(btnGreen).click(function() {
+    var originalColor = $(this).css('background-color');
+    greenBtnAudio.play();
+    $(this).css('background-color', '#7FFF00');
+    setTimeout(function() {
+      $(btnGreen).css('background-color', originalColor);
+    }, 100);
+  });
+  // button red effect
+  var redButtonEffect = $(btnRed).click(function() {
+    var originalColor = $(this).css('background-color');
+    redBtnAudio.play();
+    $(this).css('background-color', '#F08080');
+    setTimeout(function() {
+      $(btnRed).css('background-color', originalColor)
+    }, 100);
+  });
+  // button yellow effect
+  var yellowButtonEffect = $(btnYellow).click(function() {
+    var originalColor = $(this).css('background-color');
+    yellowBtnAudio.play();
+    $(this).css('background-color', '#F0E68C');
+    setTimeout(function() {
+      $(btnYellow).css('background-color', originalColor)
+    }, 100);
+  });
   
-  
+  var arr = [blueButtonEffect, redButtonEffect, greenButtonEffect, yellowButtonEffect];
+  console.log(arr);
+ //will display the loop 
+ function buttonEffects(){
+   
+   
+ }
   // start the game
   /*function startGame() { // it has a bug if clicked twice!
     $(startButton).on('click', buttonEffects);
   }
   startGame();*/
-  
-  
-/*  function changeColor() {
-    let random = Math.floor(Math.random() *effects.length);
-    gameButton.style.backgroundColor = effects[random];
-    var i;
-    for(i = 0; i < effects[random].length; i++){
-      
-    }
-  }*/
-  
-  
-  
+
+
+  /*  function changeColor() {
+      let random = Math.floor(Math.random() *effects.length);
+      gameButton.style.backgroundColor = effects[random];
+      var i;
+      for(i = 0; i < effects[random].length; i++){
+        
+      }
+    }*/
+
+
+
 
 });
-// I create an object within the buttonEffect function
-// As a result the startGame function is no longer working (start)
-// 
+
+// make a button effect function using those variables;[]
+// use the button effect variable to start the game;[]
+// make the button change its color randomly;[]
