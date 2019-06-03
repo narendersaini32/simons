e5118eddc27970f70ef1088805596c4133cebc68
Oliveira <eliaspradoprofessional@outlook.com>
2019-06-03 15:11:15 +0000

    added (hide) effect on button click

--- assets/scripts/style.js
+++ assets/scripts/style.js
@@ -1,26 +1,28 @@
-$(document).ready(function(){
-   
-  $('.ct-btn-scroll').click(function(){
-      $('html,body').animate({
-        scrollTop: $("#meBaby").offset().top},
-        'slow');
-  });
+$(document).ready(function() {
+    //will scroll down on arrow down button click
+    $('.ct-btn-scroll').click(function() {
+        $('html,body').animate({
+                scrollTop: $("#meBaby").offset().top
+            },
+            'slow');
+            $('.ct-btn-scroll').hide(1000);
+    });
 });
 
 
-var change = document.getElementById("meBaby");// add to the top
+var change = document.getElementById("meBaby"); // add to the top
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
-    for (var i = 0; i < 6; i++ ) {
+    for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 
-function changeColor(){
-  change.style.backgroundColor= getRandomColor();
+function changeColor() {
+    change.style.backgroundColor = getRandomColor();
 }
 
-setInterval(changeColor,10000);
+setInterval(changeColor, 10000);
