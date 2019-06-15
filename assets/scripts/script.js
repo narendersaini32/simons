c4c1581881f07268b89ea61bbf272ec667ce1b65
Oliveira <eliaspradoprofessional@outlook.com>
2019-06-15 14:21:53 +0000

    added comments to script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,35 +1,35 @@
-let blueBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
-let redBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
-let yellowBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
-let greenBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
-//======================//
-let organization = [];
-let playerOrder = [];
-let flash;
-let turn;
-let good;
-let compTurn;
-let intervalId;
-let strict = false;
-let noise = true;
-let on = false;
-let win;
-//======================//
+
+//variables
+let blueBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');//audio for button blue
+let redBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');//audio for button red 
+let yellowBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');//audio for button yellow
+let greenBtnAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');//audio for button green
+
+let organization = []; //Flash sequence.
+let playerOrder = [];//Select sequence.
+let flash;//Var used of flsh diplayed in the game.
+let turn;//User turn.
+let good;//Boolean to check player success.
+let compTurn;//boolean to defines cpu/player attempt.
+let intervalId;//time defined between each execution.
+let strict = false;//Boolean to allow strict mode.
+let noise = true;//Active/deactive sound.
+let on = false;//Game activation.
+let win;//Deifines the player won.
+
 // VARIABLES - DOM QUERIES
 
-const btnBlue = document.querySelector("#btnBlue");
-const btnGreen = document.querySelector("#btnGreen");
-const btnRed = document.querySelector("#btnRed");
-const btnYellow = document.querySelector("#btnYellow");
+const btnBlue = document.querySelector("#btnBlue");//Button blue
+const btnGreen = document.querySelector("#btnGreen");//Button green
+const btnRed = document.querySelector("#btnRed");//Button red 
+const btnYellow = document.querySelector("#btnYellow");//Button yellow
 
-const turnCounter = document.querySelector("#turn");
-const startButton = document.querySelector("#startButton");
-const gameButton = document.querySelector(".gameButton"); //button start (not used)
-const strictButton = document.querySelector("#strict");
-const turnOnButton = document.querySelector("#on");
+const turnCounter = document.querySelector("#turn");//Score display.
+const startButton = document.querySelector("#startButton");//Initiates the game after power is on.
+const strictButton = document.querySelector("#strict");//Triggers the strict mode.
+const turnOnButton = document.querySelector("#on");//Initiates the game.
 
-
-//Setthings buttons
+//Setthings buttons / event listeners
 strictButton.addEventListener('click', function(event) {
   if (strictButton.checked == true) {
     strict = true;
@@ -59,90 +59,6 @@ startButton.addEventListener('click', function(event) {
     
   }
 });
-
-function play() {
-  win = false;
-  organization = [];
-  playerOrder = [];
-  flash = 0;
-  intervalId = 0;
-  turn = 1;
-  turnCounter.innerHTML = 1;
-  good = true;
-  for (var i = 0; i < 20; i++) {
-    organization.push(Math.floor(Math.random() * 4) + 1);
-  }
-  compTurn = true;
-
-  intervalId = setInterval(gameTurn, 800);
-}
-
-function gameTurn() {
-
-  on = false;
-  if (flash == turn) {
-    clearInterval(intervalId);
-    compTurn = false;
-    clearColor();
-    on = true;
-  }
-  if (compTurn) {
-    clearColor();
-    setTimeout(function() {
-      if (organization[flash] == 1) first();
-      if (organization[flash] == 2) second();
-      if (organization[flash] == 3) third();
-      if (organization[flash] == 4) fourth();
-      flash++;
-    }, 200);
-  }
-}
-
-function first() {
-  if (noise) {
-    greenBtnAudio.play();
-  }
-  noise = true;
-  btnGreen.style.backgroundColor = "lightgreen";
-}
-
-function second() {
-  if (noise) {
-    redBtnAudio.play();
-  }
-  noise = true;
-  btnRed.style.backgroundColor = "tomato";
-}
-
-function third() {
-  if (noise) {
-    yellowBtnAudio.play();
-  }
-  noise = true;
-  btnYellow.style.backgroundColor = "yellow";
-}
-
-function fourth() {
-  if (noise) {
-    blueBtnAudio.play();
-  }
-  noise = true;
-  btnBlue.style.backgroundColor = "lightskyblue";
-}
-
-function clearColor() {
-  btnGreen.style.backgroundColor = "darkgreen";
-  btnRed.style.backgroundColor = "darkred";
-  btnYellow.style.backgroundColor = "goldenrod";
-  btnBlue.style.backgroundColor = "darkblue";
-}
-
-function btnLightColor() {
-  btnGreen.style.backgroundColor = "lightgreen";
-  btnRed.style.backgroundColor = "tomato";
-  btnYellow.style.backgroundColor = "yellow";
-  btnBlue.style.backgroundColor = "lightskyblue";
-}
 
 btnGreen.addEventListener('click', function(event) {
   if (on) {
@@ -196,6 +112,98 @@ btnBlue.addEventListener('click', function(event) {
   }
 })
 
+
+//Loop sequences
+
+function play() {
+  win = false;
+  organization = [];
+  playerOrder = [];
+  flash = 0;
+  intervalId = 0;
+  turn = 1;
+  turnCounter.innerHTML = 1;
+  good = true;
+  for (var i = 0; i < 20; i++) {
+    organization.push(Math.floor(Math.random() * 4) + 1);
+  }
+  compTurn = true;
+
+  intervalId = setInterval(gameTurn, 800);
+}
+
+function gameTurn() {
+
+  on = false;
+  if (flash == turn) {
+    clearInterval(intervalId);
+    compTurn = false;
+    clearColor();
+    on = true;
+  }
+  if (compTurn) {
+    clearColor();
+    setTimeout(function() {
+      if (organization[flash] == 1) first();
+      if (organization[flash] == 2) second();
+      if (organization[flash] == 3) third();
+      if (organization[flash] == 4) fourth();
+      flash++;
+    }, 200);
+  }
+}
+
+//Green button trigger
+function first() {
+  if (noise) {
+    greenBtnAudio.play();
+  }
+  noise = true;
+  btnGreen.style.backgroundColor = "lightgreen";
+}
+//Red button trigger
+function second() {
+  if (noise) {
+    redBtnAudio.play();
+  }
+  noise = true;
+  btnRed.style.backgroundColor = "tomato";
+}
+
+//Yellow button trigger
+function third() {
+  if (noise) {
+    yellowBtnAudio.play();
+  }
+  noise = true;
+  btnYellow.style.backgroundColor = "yellow";
+}
+//Blue button trigger
+function fourth() {
+  if (noise) {
+    blueBtnAudio.play();
+  }
+  noise = true;
+  btnBlue.style.backgroundColor = "lightskyblue";
+}
+
+//Color Normal state
+function clearColor() {
+  btnGreen.style.backgroundColor = "darkgreen";
+  btnRed.style.backgroundColor = "darkred";
+  btnYellow.style.backgroundColor = "goldenrod";
+  btnBlue.style.backgroundColor = "darkblue";
+}
+
+//Color Lighter state
+function btnLightColor() {
+  btnGreen.style.backgroundColor = "lightgreen";
+  btnRed.style.backgroundColor = "tomato";
+  btnYellow.style.backgroundColor = "yellow";
+  btnBlue.style.backgroundColor = "lightskyblue";
+}
+
+//player choice verification
 function check() {
   if (playerOrder[playerOrder.length - 1] !== organization[playerOrder.length - 1])
     good = false;
@@ -236,7 +244,7 @@ function check() {
   }
 
 }
-
+//Display win on score and finishs the game
 function winGame() {
   btnLightColor();
   turnCounter.innerHTML = "WIN!";
@@ -244,7 +252,7 @@ function winGame() {
   win = true;
 }
 
-// testing code
+// TESTING CODE
 
 btnBlue.addEventListener("click", function avoidBug(){
   var playerTurn = 0;
@@ -299,9 +307,3 @@ window.addEventListener("keyup", event => {
   }
 });
 
-
-/*
-fix button bug on click
-fix iphone 5 media query
-add game rules and how to win
-*/
